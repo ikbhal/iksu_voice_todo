3512ac1836d410974032552e4185864ff4f7079b
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-07-25 09:26:50 +0530

    first commit

---
+++ public/app.js
@@ -0,0 +1,20 @@
+// Get references to the voice input elements
+const voiceInput = document.getElementById('voiceInput');
+const voiceButton = document.getElementById('voiceButton');
+
+// Add event listener to the voice button
+voiceButton.addEventListener('click', startVoiceRecognition);
+
+// Function to start voice recognition
+function startVoiceRecognition() {
+  // Code to start voice recognition and handle the voice commands will be implemented here
+  // You'll need to use a speech recognition library or API to handle the voice input
+  // For this example, we'll assume you have a function called handleVoiceInput that handles the voice commands
+}
+
+// Function to handle the voice commands and update the todo list accordingly
+function handleVoiceInput(command) {
+  // Code to parse the voice command and update the todo list will be implemented here
+  // This function should handle all the commands mentioned in the requirements
+  // For example, adding todo items, deleting items, marking items as done, changing order, etc.
+}
